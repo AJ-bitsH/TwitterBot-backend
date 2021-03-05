282b5c6233548fc2c5294b6d0fea38be73464c82
DelhiBoi <52867282+DelhiBoi@users.noreply.github.com>
2021-03-06 03:25:42 +0530

    fixingmore

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -4,22 +4,24 @@ var Twitter = require("twitter");
 var config = require("../config");
 var client = new Twitter(config);
 var tweet_array = {};
-var username = "";
 
-var params = { screen_name: "narendramodi", count: 10 };
-client.get(
-  "https://api.twitter.com/1.1/statuses/user_timeline.json",
-  params,
-  function (error, tweets, response) {
-    console.log(error);
-    if (!error) {
-      tweet_array = tweets;
+function helper(params) {
+  client.get(
+    "https://api.twitter.com/1.1/statuses/user_timeline.json",
+    params,
+    function (error, tweets, response) {
+      console.log(error);
+      if (!error) {
+        tweet_array = tweets;
+      }
     }
-  }
-);
+  );
+}
 
 router.post("/", function (req, res, next) {
-  username = req.body;
+  var username = req.body;
+  var params = { screen_name: username, count: 10 };
+  helper(params);
   res.send(tweet_array);
 });
 
